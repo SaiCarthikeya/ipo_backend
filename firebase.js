13b1c8e40d243c2ce0425a0f60b69c33ecca834e
SaiCarthikeya <saicarthik180@gmail.com>
2025-01-03 21:09:59 +0530

    firebase insert

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import dotenv from 'dotenv';
+dotenv.config()
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId,
+  measurementId: process.env.FIREBASE_measurementId
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
--- index.js
+++ index.js
@@ -1,52 +1,75 @@
-require('dotenv').config()
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import firebase from './firebase.js'
+import {getDocs, addDoc, collection, getFirestore} from 'firebase/firestore'
+import dotenv from 'dotenv'
 
-const express = require('express')
-const cors = require('cors')
-const axios = require('axios');
-
+dotenv.config();
 const app = express()
 app.use(cors());
 
+const db = getFirestore(firebase);
 const port = process.env.PORT || 3000;
 
-app.get('/ipos', (req, res) => {
-    const config = {
-        method: 'get',
-        url: 'https://api.ipoalerts.in/?status=open',
-        headers: { 
-          'x-api-key': process.env.API_KEY
+const getFireStoreIpos = async (status) => {
+  try {
+    const firestoreIPOS = await getDocs(collection(db, `ipo${status}`))
+    if (firestoreIPOS.empty) {
+      return "";
+    }
+    console.log(firestoreIPOS);
+    return firestoreIPOS;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+app.get('/ipos', async (req, res) => {
+    let status = req.query.status
+    status = (status === null || status === undefined) ? "open" : status;
+
+    const currentDateTime = new Date();
+    console.log(currentDateTime);
+    const firestoreIPO = await getFireStoreIpos(status);
+    console.log(firestoreIPO)
+    console.log(status)
+    const fetchFromApi = async () => {
+      const config = {
+          method: 'get',
+          url: `https://api.ipoalerts.in/ipos?status=${status}`,
+          headers: { 
+            'x-api-key': process.env.API_KEY
+          }
+        };
+        try {
+  
+          const response = await axios.request(config);
+          const result = await response.data;
+          console.log(result)
+          await addDoc(collection(db, `ipo${status}`), {date_time: currentDateTime,ipos: result.ipos})
+          res.json({ipos: result.ipos})
+        } catch (e){
+              console.log(`Something went wrong ${e}`)
+              res.status(e.status)
         }
-      };
-    axios.request(config)
-    .then(response => {
-        const result = JSON.stringify(response.data)
-        console.log(result)
-        res.json({ipos: result})
-    }).catch(e => {
-        console.log(`Something went wrong ${e}`)
-        res.status(400)
-    })
+
+    }
+    if (firestoreIPO === "") {
+      await fetchFromApi();
+  } else {
+      const firestoreDateTime = firestoreIPO.date_time;
+      const diff = firestoreDateTime - currentDateTime;
+      const hourInMS = 3600000;
+      if (diff > hourInMS) {
+        await fetchFromApi();
+      } else {
+        res.json(firestoreIPO.ipos)
+      }
+  }
 })
 
-app.get('/ipo/:id', (req, res) => {
-    const ipo_id = req.params.id
-    const config = {
-        method: 'get',
-        url: `https://api.ipoalerts.in/ipos/:${ipo_id}`,
-        headers: { 
-          'x-api-key': process.env.API_KEY
-        }
-      };
-    axios.request(config)
-    .then(response => {
-        const result = JSON.stringify(response.data)
-        console.log(result)
-        res.json({ipo: result})
-    }).catch(e => {
-        console.log(`Something went wrong ${e}`)
-        res.status(400)
-    })
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
